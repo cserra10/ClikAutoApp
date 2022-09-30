a401b803a1b5ecf987b121ee20e538dd0e95c5cf
Cesar Serrano <cesr90@gmail.com>
2022-09-29 21:23:04 -0500

    App Center Setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ClikAutoApp
+
+IOS
+[![Build status](https://build.appcenter.ms/v0.1/apps/1a3217c5-4926-4a6c-b510-24ffcf144324/branches/main/badge)](https://appcenter.ms)
+
+Android
+[![Build status](https://build.appcenter.ms/v0.1/apps/9a16f46a-3272-4c78-a458-8e1c0019e1be/branches/main/badge)](https://appcenter.ms)
--- ios/ClikAutoApp/AppDelegate.m
+++ ios/ClikAutoApp/AppDelegate.m
@@ -3,6 +3,10 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <AppCenterReactNativeShared/AppCenterReactNativeShared.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -47,6 +51,10 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
   return YES;
 }
 
