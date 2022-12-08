aca721b85e62d5fb5813651926c27e46b4b4b693
Cesar Serrano <cesr90@gmail.com>
2022-12-08 01:48:42 -0500

    Add Readme
    Edit App.js

--- App.js
+++ App.js
@@ -75,6 +75,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Section title="ClikAuto App">Vamos a darle Putos</Section>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
---
+++ Readme.md
@@ -0,0 +1,2 @@
+## Build status
+- IOS [![Build status](https://build.appcenter.ms/v0.1/apps/e60892cc-1d03-4dbd-bc0c-0b0619e60588/branches/main/badge)](https://appcenter.ms)
